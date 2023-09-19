2ae1419e448ac191f1918048fd1bb1e21b0e5238
cbird808 <chris.bird@tamucc.edu>
2023-09-19 09:17:37 -0500

    Update runMitoFinder_roy.bash

--- scripts/runMitoFinder_roy.bash
+++ scripts/runMitoFinder_roy.bash
@@ -40,7 +40,7 @@ SCRIPTPATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 #all_samples=(${all_samples})
 all_samples=($(cat ${fqPatternFile}))
 
-sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} -p $queue -c ${threads} ${SCRIPTPATH}/runMitoFinder_Roy.sbatch ${fqPatternFile} ${fqPath} ${path2gb} ${threads} ${ramPerThreads}
+sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} -p $queue -c ${threads} ${SCRIPTPATH}/runMitoFinder_roy.sbatch ${fqPatternFile} ${fqPath} ${path2gb} ${threads} ${ramPerThreads}
 
 #for sample in ${all_samples[@]}; do
 #    sbatch -p $queue -c ${threads} ${SCRIPTPATH}/runMitoFinder.sbatch ${sample} ${fqPath} ${path2gb} ${threads} ${ramPerThread}
