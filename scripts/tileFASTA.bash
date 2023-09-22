14de43eb93c3ab1590362f2d668d4f0cb7942f7c
cbird808 <chris.bird@tamucc.edu>
2023-09-21 20:08:49 -0500

    Create tileFASTA.bash

---
+++ scripts/tileFASTA.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# this script takes sequences in a fasta file and makes shorter sequences tiled across the larger sequence
+# tiles are between 200 and 300 bp and a new tile starts every 100 bp
+
+inFILE=$1
+
+awk '
+BEGIN { RS = ">" ; FS = "\n" ; ORS = "" }
+NR > 1 {
+    header = $1
+    seq = ""
+    for (i=2; i<=NF; i++) {
+        seq = seq $i
+    }
+    len = length(seq)
+    for (i=0; i<len; i+=100) {
+        remaining = len - i
+        if (remaining < 200) {
+            break
+        }
+        subseq_len = (remaining > 300) ? 300 : remaining
+        subseq = substr(seq, i+1, subseq_len)
+        printf(">%s_%d-%d\n%s\n", header, i+1, i+subseq_len, subseq)
+        if (subseq_len < 300) {
+            break
+        }
+    }
+}
+' $inFILE
