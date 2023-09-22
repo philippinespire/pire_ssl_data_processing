3b2cfbdf89c8f2eeb29b46dc5ec9d1381b1c171a
cbird808 <chris.bird@tamucc.edu>
2023-09-21 20:24:23 -0500

    Update tileFASTA.bash

--- scripts/tileFASTA.bash
+++ scripts/tileFASTA.bash
@@ -16,14 +16,19 @@ NR > 1 {
     len = length(seq)
     i = 0
     while (i < len) {
-        subseq_len = (len - i > 300) ? 300 : len - i
-        if (len - i - subseq_len < 200 && len - i - subseq_len > 0) {
+        if (len - i < 200) {
             subseq_len = len - i
+        } else {
+            subseq_len = 300
+            if (len - i - 300 < 200 && len - i - 300 > 0) {
+                subseq_len = len - i - 100
+            }
         }
         subseq = substr(seq, i+1, subseq_len)
         printf(">%s_%d-%d\n%s\n", header, i+1, i+subseq_len, subseq)
-        i += 100  # Move 100 bp downstream for the next tile
+        i += (subseq_len == len - i) ? subseq_len : 100  # Move 100 bp downstream for the next tile, unless it's the last tile
     }
 }
 ' $inFILE
+
 
