dfbe2bb8789f0c55db5a80c28ac90768877a193c
cbird808 <chris.bird@tamucc.edu>
2023-09-23 00:33:30 -0500

    Update getSuccessesFailures.bash

--- scripts/getSuccessesFailures.bash
+++ scripts/getSuccessesFailures.bash
@@ -12,11 +12,16 @@ ls -lhtr $MitoFinderDIR | \
         grep -v "logs" | \
         sort > $MitoFinderDIR/all_mitofinder.txt
 
-ls -lhtr $MitoFinderDIR/*/*.scafSeq | \
-        cut -d "/" -f 2 | \
-        sed -e 's/  / /g' | \
-        cut -d " " -f 9 | \
-        sort > $MitoFinderDIR/successful_mitofinder.txt
+#ls -lhtr $MitoFinderDIR/*/*.scafSeq | \
+#        cut -d "/" -f 2 | \
+#        sed -e 's/  / /g' | \
+#        cut -d " " -f 9 | \
+#        sort > $MitoFinderDIR/successful_mitofinder.txt
+
+ls $MitoFinderDIR/*/*Final_Results/*_contig_*genes_NT.fasta | \
+        sed 's/\/.*//' | \
+        uniq > \
+        $MitoFinderDIR/successful_mitofinder.txt
 
 comm -23 $MitoFinderDIR/all_mitofinder.txt $MitoFinderDIR/successful_mitofinder.txt > $MitoFinderDIR/failed_mitofinder.txt
 
