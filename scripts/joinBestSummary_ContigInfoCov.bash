78a60fbcf81be6980fcb415950bff60f846b2f1c
cbird808 <chris.bird@tamucc.edu>
2023-09-24 11:48:26 -0500

    Update joinBestSummary_ContigInfoCov.bash

--- scripts/joinBestSummary_ContigInfoCov.bash
+++ scripts/joinBestSummary_ContigInfoCov.bash
@@ -19,8 +19,6 @@ tail -n+2 $rightFILE | \
 cat <(echo -e "$joined_header" | tr -s " " "\t") \
         <(join -1 1 -2 1 -t $'\t' -a 1 sorted_modified_$leftFILE sorted_modified_$rightFILE) |
         awk -F'\t' 'BEGIN {OFS="\t"} {print $11, $2, $3, $15, $18, $4, $5, $6, $7, $8, $9, $10, $13, $16, $17}' > \
-#        awk -F'\t' 'BEGIN {OFS="\t"} {print $7, $2, $3, $11, $14, $4, $5, $6, $9, $12, $13}' > \
-
         $outFILE
 
 rm sorted_modified_$leftFILE sorted_modified_$rightFILE
